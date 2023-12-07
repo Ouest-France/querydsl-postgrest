40e202484aff37f77a7e6b2892681b58738a872e
Arnaud <arnaud.thorel@ouest-france.fr>
2023-12-07 10:21:42 +0100

    feat(prefer-headers): add prefer headers constants with documentation

--- src/test/java/fr/ouestfrance/querydsl/postgrest/app/PostRepository.java
+++ src/test/java/fr/ouestfrance/querydsl/postgrest/app/PostRepository.java
@@ -6,13 +6,14 @@ import fr.ouestfrance.querydsl.postgrest.PostgrestRepository;
 import fr.ouestfrance.querydsl.postgrest.annotations.Header;
 import fr.ouestfrance.querydsl.postgrest.annotations.PostgrestConfiguration;
 import fr.ouestfrance.querydsl.postgrest.annotations.Select;
+import fr.ouestfrance.querydsl.postgrest.model.Prefer;
 
 import static fr.ouestfrance.querydsl.postgrest.annotations.Header.Method.UPSERT;
 
 @PostgrestConfiguration(resource = "posts")
 @Select("authors(*)")
-@Header(key = "Prefer", value = "return=representation")
-@Header(key = "Prefer", value = {"tx=rollback", "resolution=merge-duplicates"}, methods = UPSERT)
+@Header(key = Prefer.HEADER, value = Prefer.Return.REPRESENTATION)
+@Header(key = Prefer.HEADER, value = Prefer.Resolution.MERGE_DUPLICATES, methods = UPSERT)
 public class PostRepository extends PostgrestRepository<Post> {
 
     public PostRepository(PostgrestClient client) {
