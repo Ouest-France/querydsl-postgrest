53e6f434a69d9a47337851d3d6ba93706a51fa9a
antoinebesnardof <156302250+antoinebesnardof@users.noreply.github.com>
2024-03-13 16:52:37 +0100

    fix/29-encoding Ampersand not encoded when contained in a search criteria value (#30)

--- querydsl-postgrest-resttemplate-adapter/src/main/java/fr/ouestfrance/querydsl/postgrest/PostgrestRestTemplate.java
+++ querydsl-postgrest-resttemplate-adapter/src/main/java/fr/ouestfrance/querydsl/postgrest/PostgrestRestTemplate.java
@@ -6,8 +6,6 @@ import fr.ouestfrance.querydsl.postgrest.model.Range;
 import fr.ouestfrance.querydsl.postgrest.model.RangeResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.reflect.TypeUtils;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -17,9 +15,10 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.HashMap;
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 import static fr.ouestfrance.querydsl.postgrest.ParametrizedTypeUtils.listRef;
@@ -36,23 +35,19 @@ public class PostgrestRestTemplate implements PostgrestClient {
      */
     private final RestTemplate restTemplate;
 
-    /**
-     * Postgrest restTemplate adapter
-     *
-     * @param restTemplate restTemplate
-     * @return PostgrestRestTemplate implementation
-     */
-    public static PostgrestRestTemplate of(RestTemplate restTemplate) {
-        return new PostgrestRestTemplate(restTemplate);
+    private final String baseUrl;
+
+    public static PostgrestRestTemplate of(RestTemplate restTemplate, String baseUrl) {
+        return new PostgrestRestTemplate(restTemplate, baseUrl);
     }
 
     @Override
     public <T> RangeResponse<T> search(String resource, Map<String, List<String>> params,
                                        Map<String, List<String>> headers, Class<T> clazz) {
-        ResponseEntity<List<T>> response = restTemplate.exchange(restTemplate.getUriTemplateHandler()
-                        .expand(UriComponentsBuilder.fromPath(resource)
-                                .queryParams(toMultiMap(params)).build().toString(), new HashMap<>()), HttpMethod.GET,
+        ResponseEntity<List<T>> response = restTemplate.exchange(
+                getUri(resource, params), HttpMethod.GET,
                 new HttpEntity<>(null, toHeaders(headers)), listRef(clazz));
+
         // Retrieve result headers
         return Optional.of(response)
                 .map(HttpEntity::getBody)
@@ -65,30 +60,31 @@ public class PostgrestRestTemplate implements PostgrestClient {
 
     @Override
     public <T> BulkResponse<T> post(String resource, List<Object> value, Map<String, List<String>> headers, Class<T> clazz) {
-        ResponseEntity<List<T>> response = restTemplate.exchange(resource, HttpMethod.POST, new HttpEntity<>(value, toHeaders(headers)), listRef(clazz));
+        ResponseEntity<List<T>> response = restTemplate.exchange(getUri(resource), HttpMethod.POST, new HttpEntity<>(value, toHeaders(headers)), listRef(clazz));
         return toBulkResponse(response);
     }
 
 
     @Override
     public <T> BulkResponse<T> patch(String resource, Map<String, List<String>> params, Object value, Map<String, List<String>> headers, Class<T> clazz) {
-        ResponseEntity<List<T>> response = restTemplate.exchange(restTemplate.getUriTemplateHandler()
-                        .expand(UriComponentsBuilder.fromPath(resource).queryParams(toMultiMap(params)).build().toString(), new HashMap<>()),
+        ResponseEntity<List<T>> response = restTemplate.exchange(
+                getUri(resource, params),
                 HttpMethod.PATCH, new HttpEntity<>(value, toHeaders(headers)), listRef(clazz));
         return toBulkResponse(response);
     }
 
     @Override
     public <T> BulkResponse<T> delete(String resource, Map<String, List<String>> params, Map<String, List<String>> headers, Class<T> clazz) {
-        ResponseEntity<List<T>> response = restTemplate.exchange(restTemplate.getUriTemplateHandler().expand(UriComponentsBuilder.fromPath(resource)
-                .queryParams(toMultiMap(params)).build().toString(), new HashMap<>()), HttpMethod.DELETE, new HttpEntity<>(null, toHeaders(headers)), listRef(clazz));
+        ResponseEntity<List<T>> response = restTemplate.exchange(
+                getUri(resource, params),
+                HttpMethod.DELETE, new HttpEntity<>(null, toHeaders(headers)), listRef(clazz));
         return toBulkResponse(response);
     }
 
     @Override
     public List<CountItem> count(String resource, Map<String, List<String>> map) {
-        return restTemplate.exchange(restTemplate.getUriTemplateHandler().expand(UriComponentsBuilder.fromPath(resource)
-                .queryParams(toMultiMap(map)).build().toString(), new HashMap<>()), HttpMethod.GET, new HttpEntity<>(null, new HttpHeaders()), listRef(CountItem.class)).getBody();
+        return restTemplate.exchange(
+                getUri(resource, map), HttpMethod.GET, new HttpEntity<>(null, new HttpHeaders()), listRef(CountItem.class)).getBody();
     }
 
 
@@ -99,5 +95,17 @@ public class PostgrestRestTemplate implements PostgrestClient {
     private static HttpHeaders toHeaders(Map<String, List<String>> headers) {
         return new HttpHeaders(toMultiMap(headers));
     }
+    private URI getUri(String resource) {
+        return getUri(resource, null);
+    }
+
+    private URI getUri(String resource, Map<String, List<String>> params) {
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(baseUrl).path(resource);
+        if (Objects.nonNull(params)) {
+            uriBuilder = uriBuilder.queryParams(toMultiMap(params));
+        }
+        return uriBuilder.build().encode().toUri();
+    }
+
 
 }
